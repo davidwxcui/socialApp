240fdb33af527c9907e8930e16adb965aac395b6
David Cui <davidcui6688@gmail.com>
2024-05-27 01:23:15 -0700

    server file

--- server/index.js
+++ server/index.js
@@ -32,7 +32,12 @@ app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://your-production-domain.com'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true // Enable credentials (if you need to send cookies or authorization headers)
+}));
 /* FILE STORAGE */
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
